60312913ae03ae367bfecbf749f76627ed3aa9b0
1 <g.ponsard@simplon-charleville.fr>
2019-04-01 16:55:20 +0200

    ajout slider

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+
+
+<section id="slideshow">
+		
+	<div class="container">
+		<div class="c_slider"></div>
+		<div class="slider">
+			<figure>
+				<img src="img/01.jpg" alt="" width="640" height="310" />
+				<figcaption>The mirror of soul</figcaption>
+
+			</figure><!--
+			--><figure>
+				<img src="img/02.jpg" alt="" width="640" height="310" />
+				<figcaption>Let's cross that bridge when we come to it</figcaption>
+			</figure><!--
+			--><figure>
+				<img src="img/03.jpg" alt="" width="640" height="310" />
+				<figcaption>Sushi<em>(do)</em> time</figcaption>
+
+			</figure><!--
+			--><figure>
+				<img src="img/04.jpg" alt="" width="640" height="310" />
+				<figcaption>Waking Life</figcaption>
+			</figure>
+		</div>
+	</div>
+		
+	<span id="timeline"></span>
+</section>
+
+
+
+
+    
+</body>
+</html>
