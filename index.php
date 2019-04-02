4f4874b2b78a076aa596574467ccf735af29d30b
1 <g.ponsard@simplon-charleville.fr>
2019-04-02 18:23:25 +0200

    ajout nav bar, paragraphe, galerie, footer 1

--- index.php
+++ index.php
@@ -1,47 +1,128 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Document</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Document</title>
 </head>
+
 <body>
 
+	<!-- navbar -->
+	<div class="header">
+		<ul>
+			<li><a class="active" href="#home">Home</a></li>
+			<li><a href="#news">News</a></li>
+			<li><a href="#contact">Contact</a></li>
+		</ul>
+	</div>
 
-<section id="slideshow">
-		
-	<div class="container">
-		<div class="c_slider"></div>
-		<div class="slider">
-			<figure>
-				<img src="img/01.jpg" alt="" width="640" height="310" />
-				<figcaption>The mirror of soul</figcaption>
+	<!-- slider -->
+	<div class="slider">
+		<figure>
+			<img src="img/01.jpg" alt>
+		</figure>
+		<figure>
+			<img src="img/02.jpg" alt>
+		</figure>
+		<figure>
+			<img src="img/03.jpg" alt>
+		</figure>
+		<figure>
+			<img src="img/04.jpg" alt>
+		</figure>
+		<figure>
+			<img src="img/05.jpg" alt>
+		</figure>
+	</div>
 
-			</figure><!--
-			--><figure>
-				<img src="img/02.jpg" alt="" width="640" height="310" />
-				<figcaption>Let's cross that bridge when we come to it</figcaption>
-			</figure><!--
-			--><figure>
-				<img src="img/03.jpg" alt="" width="640" height="310" />
-				<figcaption>Sushi<em>(do)</em> time</figcaption>
+	<!-- paragraphe image + texte -->
+	<div class="paragraphe">
+		<div class="img_paragraphe">
+			<img src="img/01.jpg" alt>
+		</div>
+		<div class="texte_paragraphe">
+			<span>
 
-			</figure><!--
-			--><figure>
-				<img src="img/04.jpg" alt="" width="640" height="310" />
-				<figcaption>Waking Life</figcaption>
-			</figure>
+				Ciliciam vero, quae Cydno amni exultat, Tarsus nobilitat, urbs perspicabilis hanc condidisse Perseus
+				memoratur, Iovis filius et Danaes, vel certe ex Aethiopia profectus Sandan quidam nomine vir opulentus
+				et nobilis et Anazarbus auctoris vocabulum referens, et Mopsuestia vatis illius domicilium Mopsi, quem a
+				conmilitio Argonautarum cum aureo vellere direpto redirent, errore abstractum delatumque ad Africae
+				litus mors repentina consumpsit, et ex eo cespite punico tecti manes eius heroici dolorum varietati
+				medentur plerumque sospitales.
+
+				Ciliciam vero, quae Cydno amni exultat, Tarsus nobilitat, urbs perspicabilis hanc condidisse Perseus
+				memoratur, Iovis filius et Danaes, vel certe ex Aethiopia profectus Sandan quidam nomine vir opulentus
+				et nobilis et Anazarbus auctoris vocabulum referens, et Mopsuestia vatis illius domicilium Mopsi, quem a
+				conmilitio Argonautarum cum aureo vellere direpto redirent, errore abstractum delatumque ad Africae
+				litus mors repentina consumpsit, et ex eo cespite punico tecti manes eius heroici dolorum varietati
+				medentur plerumque sospitales.
+
+</span>
 		</div>
 	</div>
-		
-	<span id="timeline"></span>
-</section>
 
+	<!-- parallaxe -->
+	<div class="parallax-window" data-parallax="scroll" data-image-src="img/03.jpg"></div>
 
+	<!-- galerie -->
+	<div class="galerie">
+		<div class="image1">
+			<img src="img/01.jpg" alt>
+		</div>
+		<div class="image2">
+			<img src="img/01.jpg" alt>
+		</div>
+		<div class="image3">
+			<img src="img/01.jpg" alt>
+		</div>
+		<div class="image4">
+			<img src="img/01.jpg" alt>
+		</div>
+		<div class="image5">
+			<img src="img/01.jpg" alt>
+		</div>
+		<div class="image6">
+			<img src="img/01.jpg" alt>
+		</div>
+		<div class="image7">
+			<img src="img/01.jpg" alt>
+		</div>
+		<div class="image8">
+			<img src="img/01.jpg" alt>
+		</div>
+	</div>
 
+	<!-- footer 1 -->
+	<div class="footer1">
+		<div class="liens_footer">
+			<span>blbalblbalblbalblballlaa</span>
+			<span>blbalblbalblbalblballlaa</span>
+			<span>blbalblbalblbalblballlaa</span>
+			<span>blbalblbalblbalblballlaa</span>
+			<span>blbalblbalblbalblballlaa</span>
+		</div>
+		<div class="liens_footer">
+		<span>blbalblbalblbalblballlaa</span>
+			<span>blbalblbalblbalblballlaa</span>
+			<span>blbalblbalblbalblballlaa</span>
+			<span>blbalblbalblbalblballlaa</span>
+			<span>blbalblbalblbalblballlaa</span>
+		</div>
+		<div class="liens_footer">
+			<span>blbalblbalblbalblballlaablblblblblblblblblblblblblblblbl</span>
+			<span>blbalblbalblbalblballlaablblblblblblblblblblblblblblblbl</span>
+			<span>blbalblbalblbalblballlaablblblblblblblblblblblblblblblbl</span>
+			<span>blbalblbalblbalblballlaablblblblblblblblblblblblblblblbl</span>
+			<span>blbalblbalblbalblballlaablblblblblblblblblblblblblblblbl</span>
+		</div>
+	</div>
 
-    
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
 </body>
+
 </html>
