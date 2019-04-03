5ce6d193c79afce17c7605a77a72b0e457d88e2d
1 <g.ponsard@simplon-charleville.fr>
2019-04-03 12:05:49 +0200

    ajout des footers

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
 	<meta charset="UTF-8">
@@ -12,32 +12,34 @@
 <body>
 
 	<!-- navbar -->
-	<div class="header">
+	<nav>
 		<ul>
 			<li><a class="active" href="#home">Home</a></li>
 			<li><a href="#news">News</a></li>
 			<li><a href="#contact">Contact</a></li>
 		</ul>
-	</div>
+	</nav>
 
 	<!-- slider -->
-	<div class="slider">
-		<figure>
-			<img src="img/01.jpg" alt>
-		</figure>
-		<figure>
-			<img src="img/02.jpg" alt>
-		</figure>
-		<figure>
-			<img src="img/03.jpg" alt>
-		</figure>
-		<figure>
-			<img src="img/04.jpg" alt>
-		</figure>
-		<figure>
-			<img src="img/05.jpg" alt>
-		</figure>
-	</div>
+	<header>
+		<div class="slider">
+			<figure>
+				<img src="img/01.jpg" alt>
+			</figure>
+			<figure>
+				<img src="img/02.jpg" alt>
+			</figure>
+			<figure>
+				<img src="img/03.jpg" alt>
+			</figure>
+			<figure>
+				<img src="img/04.jpg" alt>
+			</figure>
+			<figure>
+				<img src="img/05.jpg" alt>
+			</figure>
+		</div>
+	</header>
 
 	<!-- paragraphe image + texte -->
 	<div class="paragraphe">
@@ -45,7 +47,8 @@
 			<img src="img/01.jpg" alt>
 		</div>
 		<div class="texte_paragraphe">
-			<span>
+			<h1>TITRE 1</h1>
+			<p>
 
 				Ciliciam vero, quae Cydno amni exultat, Tarsus nobilitat, urbs perspicabilis hanc condidisse Perseus
 				memoratur, Iovis filius et Danaes, vel certe ex Aethiopia profectus Sandan quidam nomine vir opulentus
@@ -53,7 +56,9 @@
 				conmilitio Argonautarum cum aureo vellere direpto redirent, errore abstractum delatumque ad Africae
 				litus mors repentina consumpsit, et ex eo cespite punico tecti manes eius heroici dolorum varietati
 				medentur plerumque sospitales.
-
+			</p>
+			<H2>Titre 2</H2>
+			<p>
 				Ciliciam vero, quae Cydno amni exultat, Tarsus nobilitat, urbs perspicabilis hanc condidisse Perseus
 				memoratur, Iovis filius et Danaes, vel certe ex Aethiopia profectus Sandan quidam nomine vir opulentus
 				et nobilis et Anazarbus auctoris vocabulum referens, et Mopsuestia vatis illius domicilium Mopsi, quem a
@@ -61,7 +66,7 @@
 				litus mors repentina consumpsit, et ex eo cespite punico tecti manes eius heroici dolorum varietati
 				medentur plerumque sospitales.
 
-</span>
+			</p>
 		</div>
 	</div>
 
@@ -69,7 +74,11 @@
 	<div class="parallax-window" data-parallax="scroll" data-image-src="img/03.jpg"></div>
 
 	<!-- galerie -->
+	<div class="galerie_title">
+		<h1>GALERIE</h1>
+	</div>
 	<div class="galerie">
+
 		<div class="image1">
 			<img src="img/01.jpg" alt>
 		</div>
@@ -98,28 +107,33 @@
 
 	<!-- footer 1 -->
 	<div class="footer1">
-		<div class="liens_footer">
-			<span>blbalblbalblbalblballlaa</span>
-			<span>blbalblbalblbalblballlaa</span>
-			<span>blbalblbalblbalblballlaa</span>
-			<span>blbalblbalblbalblballlaa</span>
-			<span>blbalblbalblbalblballlaa</span>
+		<div class="box">
+			<h3>titre</h3>
+			<a href="#">blbalblbalblbalblballlaa</a><br>
+			<a href="#">blbalblbalblbalblballlaa</a><br>
+			<a href="#">blbalblbalblbalblballlaa</a><br>
+			<a href="#">blbalblbalblbalblballlaa</a><br>
 		</div>
-		<div class="liens_footer">
-		<span>blbalblbalblbalblballlaa</span>
-			<span>blbalblbalblbalblballlaa</span>
-			<span>blbalblbalblbalblballlaa</span>
-			<span>blbalblbalblbalblballlaa</span>
-			<span>blbalblbalblbalblballlaa</span>
+		<div class="box">
+			<h3>titre</h3>
+			<a href="#">blbalblbalblbalblballlaa</a><br>
+			<a href="#">blbalblbalblbalblballlaa</a><br>
+			<a href="#">blbalblbalblbalblballlaa</a><br>
+			<a href="#">blbalblbalblbalblballlaa</a><br>
 		</div>
-		<div class="liens_footer">
-			<span>blbalblbalblbalblballlaablblblblblblblblblblblblblblblbl</span>
-			<span>blbalblbalblbalblballlaablblblblblblblblblblblblblblblbl</span>
-			<span>blbalblbalblbalblballlaablblblblblblblblblblblblblblblbl</span>
-			<span>blbalblbalblbalblballlaablblblblblblblblblblblblblblblbl</span>
-			<span>blbalblbalblbalblballlaablblblblblblblblblblblblblblblbl</span>
+		<div class="box">
+			<h3>titre</h3>
+			<a href="#">blbalblbalblbalblballlaablblblblblblblblblblblblblblblbl</a><br>
+			<a href="#">blbalblbalblbalblballlaablblblblblblblblblblblblblblblbl</a><br>
+			<a href="#">blbalblbalblbalblballlaablblblblblblblblblblblblblblblbl</a><br>
+			<a href="#">blbalblbalblbalblballlaablblblblblblblblblblblblblblblbl</a><br>
 		</div>
 	</div>
+
+	<!-- footer 2 -->
+	<div class="footer2">
+	<h3>Copyright © 2019 - Gaël Ponsard</h3>
+</div>
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
